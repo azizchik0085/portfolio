249ffc3ee6aa980fa7d4f493a21b79c734e3917b
MacBook Pro <macbookpro@192.168.10.103>
2022-11-17 10:01:26 +0500

    changed profile component

--- src/portfolyo/Partfolio.js
+++ src/portfolyo/Partfolio.js
@@ -10,6 +10,10 @@ const Portfolio = () =>{
         <>
             <Reveal effct="animNav">
                 <NavSec />
+
+
+
+
             </Reveal>
             <Reveal effct="animNav">
                 <PageInfo />
